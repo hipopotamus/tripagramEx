b89b9f6bf1251dd57a7df50e8e25e9cef39e265c
hipopotamus <qkqntjddn@gmail.com>
2023-04-11 19:08:33 +0900

    스프링 프로젝트 추가

---
+++ tripagramEx/src/main/java/tripagramex/TripagramExApplication.java
@@ -0,0 +1,13 @@
+package tripagramex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripagramExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripagramExApplication.class, args);
+	}
+
+}
