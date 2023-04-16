bb85597491783c16047a5193bc830dd44f8817b9
hipopotamus <qkqntjddn@gmail.com>
2023-04-16 16:09:15 +0900

    [Infra] Security Hndler 추가

---
+++ tripagramEx/src/main/java/tripagramex/global/security/handler/AccountAuthenticationEntryPoint.java
@@ -0,0 +1,29 @@
+package tripagramex.global.security.handler;
+
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import tripagramex.global.exception.ExceptionCode;
+import tripagramex.global.exception.dto.ErrorResponse;
+
+import java.io.IOException;
+
+public class AccountAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        ExceptionCode unAuthentication = ExceptionCode.UN_AUTHENTICATION;
+        ErrorResponse unAuthException =
+                new ErrorResponse("UnAuthentication", unAuthentication.getMessage(), unAuthentication.getCode());
+
+        String authenticationExJson = new Gson().toJson(unAuthentication);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(authenticationExJson);
+    }
+}
