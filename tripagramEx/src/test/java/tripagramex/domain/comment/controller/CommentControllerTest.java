39ba0d5373bd5e898d0beea2f4ecc886601b9274
hipopotamus <qkqntjddn@gmail.com>
2024-01-08 16:54:21 +0900

    [Test] 댓글 삭제 & 게시물 댓글 조회 문서화

--- tripagramEx/src/test/java/tripagramex/domain/comment/controller/CommentControllerTest.java
+++ tripagramEx/src/test/java/tripagramex/domain/comment/controller/CommentControllerTest.java
@@ -20,13 +20,14 @@ import tripagramex.domain.comment.dto.UpdateRequest;
 import tripagramex.global.security.authentication.UserAccount;
 import tripagramex.global.security.jwt.JwtProcessor;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static tripagramex.util.ApiDocumentUtils.getRequestPreProcessor;
 import static tripagramex.util.ApiDocumentUtils.getResponsePreProcessor;
@@ -185,6 +186,76 @@ class CommentControllerTest {
                         )
                 ));
 
+    }
+
+    @Test
+    @DisplayName("댓글 삭제_성공")
+    public void deleteComment_Success() throws Exception {
+        //given
+        Long accountId = 10001L;
+        Account account = accountRepository.findById(accountId).get();
+        String jwt = "Bearer " + jwtProcessor.createAuthJwtToken(new UserAccount(account));
+        Long commentId = 40001L;
+
+        //when
+        ResultActions delete = mockMvc.perform(
+                delete("/comments/{commentId}", commentId)
+                        .header("Authorization", jwt));
+
+        //then
+        delete
+                .andExpect(status().isOk())
+                .andDo(document(
+                        "deleteComment",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        requestHeaders(
+                                headerWithName("Authorization").description("JWT")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("게시물 댓글 조회_성공")
+    public void readBoardComments() throws Exception {
+        //given
+        Long boardId = 30001L;
+
+        //when
+        ResultActions read = mockMvc.perform(
+                get("/comments/board/{boardId}", boardId));
+
+        //then
+        read
+                .andExpect(status().isOk())
+                .andDo(document(
+                        "readBoardComment",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        pathParameters(
+                                parameterWithName("boardId").description("Board 식별자")
+                        ),
+                        formParameters(
+                                parameterWithName("page").description("페이지").optional(),
+                                parameterWithName("size").description("페이지 크기").optional(),
+                                parameterWithName("lastCommentId").description("마지막으로 조회된 댓글 식별자").optional()
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("content[]").type(JsonFieldType.ARRAY).description("댓글 목록"),
+                                        fieldWithPath("content[].commentId").type(JsonFieldType.NUMBER).description("댓글 식별자"),
+                                        fieldWithPath("content[].account").type(JsonFieldType.OBJECT).description("댓글 생성자"),
+                                        fieldWithPath("content[].account.accountId").type(JsonFieldType.NUMBER).description("댓글 생성자 식별자"),
+                                        fieldWithPath("content[].account.nickname").type(JsonFieldType.STRING).description("댓글 생성자 닉네임"),
+                                        fieldWithPath("content[].content").type(JsonFieldType.STRING).description("댓글 내용"),
+                                        fieldWithPath("content[].modifiedAt").type(JsonFieldType.STRING).description("댓글 수정 일자"),
+                                        fieldWithPath("content[].subCommentSize").type(JsonFieldType.NUMBER).description("대댓글 갯수"),
+                                        fieldWithPath("sliceNumber").type(JsonFieldType.NUMBER).description("현재 슬라이스 번호"),
+                                        fieldWithPath("size").type(JsonFieldType.NUMBER).description("슬라이스 사이즈"),
+                                        fieldWithPath("hasNext").type(JsonFieldType.BOOLEAN).description("다음 슬라이스 존재 여부"),
+                                        fieldWithPath("numberOfElements").type(JsonFieldType.NUMBER).description("현재 슬라이스의 게시글 수")
+                        )
+                )));
     }
 
 }
